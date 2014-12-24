d0397cf4b71cf08133f84b98cd3a156ff9f099e0
Peter Arentsen <peter.arentsen@quicknet.nl>
2014-12-24 02:12:52 +0100

    Update enum property gem for rails 4.2

--- lib/activerecord_enum.rb
+++ lib/activerecord_enum.rb
@@ -7,60 +7,32 @@ require 'connection_adapters/mysql2' if defined?( Mysql2 )
 
 module ActiveRecord
   module ConnectionAdapters
-    class Column
-      def initialize_with_enum name, default, sql_type=nil, *args
-        initialize_without_enum name, default, sql_type, *args
-        @type = simplified_type_with_enum sql_type
-        @limit = extract_limit_with_enum sql_type
-        @default = extract_default_with_enum default
+    class AbstractMysqlAdapter
+      protected
+      def initialize_type_map_with_enum(m)
+        initialize_without_enum(m)
+        register_enum_type(m, %r(^enum)i)
       end
-      alias_method :initialize_without_enum, :initialize
-      alias_method :initialize, :initialize_with_enum
 
-      def simplified_type_with_enum field_type
-        if field_type =~ /enum|set/i
-          $&.to_sym
-        else
-          field_type
+      alias_method :initialize_without_enum, :initialize_type_map
+      alias_method :initialize_type_map, :initialize_type_map_with_enum
+
+      def register_enum_type(mapping, key)
+        mapping.register_type(key) do |sql_type|
+          if sql_type =~ /(?:enum|set)\(([^)]+)\)/i
+            limit = $1.scan( /'([^']*)'/ ).flatten
+            Type::Enum.new(limit: limit)
+          end
         end
-      end
-
-      def extract_limit_with_enum field_type
-        if field_type =~ /(?:enum|set)\(([^)]+)\)/i
-          $1.scan( /'([^']*)'/ ).flatten
-        else
-          extract_limit field_type
-        end
-      end
-
-      def extract_default_with_enum default
-        if type == :set
-          default.split "," if default.present?
-        else
-          extract_default default
-        end
-      end
-
-      def set?
-        type == :set
-      end
-
-      def enum?
-        type == :enum
       end
     end
   end
 end
 module ActiveRecord
   module Type
-    class Enum
+    class Enum < Type::Value
       def type
         :enum
-      end
-
-      def initialize(options = {})
-        options.assert_valid_keys(:limit)
-        @limit = options[:limit]
       end
     end
   end
